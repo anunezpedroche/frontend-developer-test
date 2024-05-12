50f3cef224977ba51218d1028a09e291850ed07c
Adrián Núñez <anunezpedroche@gmail.com>
2024-05-12 18:15:36 +0200

    add comments section

--- src/pages/post/components/comments-section/index.tsx
+++ src/pages/post/components/comments-section/index.tsx
@@ -1,3 +1,4 @@
+import "./index.css";
 import { useGetCommentsByPostIdQuery } from "../../../../services/posts";
 
 export default function CommentsSection({ id }: { id: number }) {
@@ -5,13 +6,13 @@ export default function CommentsSection({ id }: { id: number }) {
 
   return (
     data && (
-      <section className="comments__container">
-        <h3>Comments</h3>
+      <section>
+        <h3 className="comments__title">Comments</h3>
         <ul className="comments__list">
           {data.map((comment) => (
             <li key={comment.id} className="comment__item">
-              <h4 className="comment__title">{comment.email}</h4>
               <p className="comment__body">{comment.body}</p>
+              <p className="comment__user-info">{`Comment written by ${comment.email} | ${comment.name}`}</p>
             </li>
           ))}
         </ul>
