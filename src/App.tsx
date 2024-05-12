308591a118824cb618b0bf3152902a375e832ea6
Adrián Núñez <anunezpedroche@gmail.com>
2024-05-12 08:31:51 +0200

    add basic router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Provider } from "react-redux";
 import HomePage from "./pages/home";
 import { store } from "./stores";
+import AppRouter from "./router";
 
 function App() {
   return (
     <Provider store={store}>
-      <HomePage />
+      <AppRouter />
     </Provider>
   );
 }
---
+++ src/router/index.tsx
@@ -0,0 +1,12 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import HomePage from "../pages/home";
+
+export default function AppRouter() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
+}
